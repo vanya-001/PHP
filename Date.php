7ea567e4a3d793a2ba3a0fff2839fca3ccce9b4f
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-07-21 15:18:12 +0530

    Date and time functions

--- Date.php
+++ Date.php
@@ -1,4 +1,14 @@
 <?php
     // Date function
-    
+    date_default_timezone_set("ASIA/KOLKATA");
+    echo "<pre>";
+    print_r(getdate());
+
+    $d = getdate();
+    echo $d['hours']."<br />";
+
+    // date() => formats a local date and time and returns the formatted date string.
+    echo date('d-D')."<br/>";
+    // time() => returns the current time in the number of seconds the Unix Epoch.
+    echo time()."<br/>";
 ?>
---
+++ Include.php
@@ -0,0 +1,6 @@
+<!-- 
+    include()
+    include_once()
+    require()
+    require_once()
+-->
